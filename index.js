0046e64837438ec7e390b7c6f7b8fbc32642d52c
Kaisar <kaisar.bolatkan@gmail.com>
2024-11-11 19:52:43 +0500

    exam tasks added

---
+++ index.js
@@ -0,0 +1,26 @@
+// let san = +prompt("1 zhane 12 sanyn arasynan sandi engiziniz")
+
+// if (san==3 || san==4 || san==5) {
+//     alert("Spring")
+// } else if (san==6 || san==7 || san==8){
+//     alert("Summer")
+// }else if (san==9 || san==10 || san==11){
+//     alert("Autumn")
+// } else if (san==12 || san==1 || san==2){
+//     alert("Winter")
+// }
+
+let san1 = +prompt("birinshi sandy engiziniz")
+
+let san2 = +prompt("ekinshi sandy engiziniz")
+
+switch (true) {
+    case (a > b):
+        alert("a san b dan ulken")
+        break;
+    case(b > a):
+        alert("b san a dan ulken")
+        break;   
+    default:alert("egerde eki san ten")
+        break;
+}
